dfa6bee09b8165ac08acdfda9594e69ed812367d
rubenDominguezPresa <rubendominguezpresa@hotmail.com>
2016-11-05 20:17:06 +0100

    refactor tdd

--- tdd/lib/fizz_buzz.rb
+++
@@ -1,9 +0,0 @@
-class FizzBuzz
-	def initialize
-		
-	end
-	def pr(num)
-		num.to_i%3==0 ? "FIZZ" : "BUZZ"
-	end
-
-end
--- tdd/spec/fizz_buzz_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/fizz_buzz"
-
-require "spec_helper"
-
-describe "fizz_buzz" do
-it "3 prints FIZZ" do
-    expect(FizzBuzz.new.pr(3)).to eq("FIZZ")
-  end
-end
